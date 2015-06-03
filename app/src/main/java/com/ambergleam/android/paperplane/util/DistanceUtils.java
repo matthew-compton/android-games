2e0f32f8269f61df4eff9b03c85ef5834de0da2d
matthew-compton <compton@bignerdranch.com>
2015-06-03 18:03:59 -0400

    Switched to km

--- app/src/main/java/com/ambergleam/android/paperplane/util/DistanceUtils.java
+++ app/src/main/java/com/ambergleam/android/paperplane/util/DistanceUtils.java
@@ -4,11 +4,12 @@ import java.text.DecimalFormat;
 
 public class DistanceUtils {
 
-    private static final String DECIMAL_FORMAT_DISTANCE = "#,###,###";
+    private static final String DECIMAL_FORMAT_DISTANCE = "#,###,##0.000";
 
-    public static String formatDistance(int distance_ft) {
+    public static String formatDistance(int distance_m) {
+        double distance_km = distance_m / 1000.0;
         DecimalFormat formatter = new DecimalFormat(DECIMAL_FORMAT_DISTANCE);
-        String formattedDistance = formatter.format(distance_ft).concat(" ft");
+        String formattedDistance = formatter.format(distance_km).concat(" km");
         return formattedDistance;
     }
 
--- app/src/test/java/com/ambergleam/android/paperplane/util/DistanceUtilsTest.java
+++ app/src/test/java/com/ambergleam/android/paperplane/util/DistanceUtilsTest.java
@@ -8,36 +8,32 @@ public class DistanceUtilsTest {
 
     @Test
     public void testDistanceUtilsForLargeNumber() {
-        // 1,200,300ft
-        int testDistance = 1200300;
-        String expectedDistanceString = "1,200,300 ft";
+        int testDistance = 1200300001;
+        String expectedDistanceString = "1,200,300.001 km";
         String actualDistanceString = DistanceUtils.formatDistance(testDistance);
         assertThat(actualDistanceString).isEqualTo(expectedDistanceString);
     }
 
     @Test
     public void testDistanceUtilsForMediumNumber() {
-        // 1,200ft
-        int testDistance = 1200;
-        String expectedDistanceString = "1,200 ft";
+        int testDistance = 1200050;
+        String expectedDistanceString = "1,200.050 km";
         String actualDistanceString = DistanceUtils.formatDistance(testDistance);
         assertThat(actualDistanceString).isEqualTo(expectedDistanceString);
     }
 
     @Test
     public void testDistanceUtilsForSmallNumber() {
-        // 320ft
         int testDistance = 320;
-        String expectedDistanceString = "320 ft";
+        String expectedDistanceString = "0.320 km";
         String actualDistanceString = DistanceUtils.formatDistance(testDistance);
         assertThat(actualDistanceString).isEqualTo(expectedDistanceString);
     }
 
     @Test
     public void testDistanceUtilsForZero() {
-        // 0ft
         int testDistance = 0;
-        String expectedDistanceString = "0 ft";
+        String expectedDistanceString = "0.000 km";
         String actualDistanceString = DistanceUtils.formatDistance(testDistance);
         assertThat(actualDistanceString).isEqualTo(expectedDistanceString);
     }
